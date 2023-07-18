5629e66da188fca11e3b879f4d69db6b0d17fbd7
d4n13ls1104 <=>
2023-07-17 21:19:05 -0400

    use TTF_SizeText to render text at consistent size without distortion

--- src/main.c
+++ src/main.c
@@ -33,19 +33,13 @@ int main() {
 			printf("Renderer could not be initialized.\n"
 					"SDL_Error: %s\n", SDL_GetError());
 		} else {
-			TTF_Font* font = TTF_OpenFont("jb_mono.ttf", 24);
+			TTF_Font* font = TTF_OpenFont("jb_mono.ttf", 16);
 			if(!font) {
 				printf("Font failed to load. TTF_Error: %s\n", TTF_GetError());
 				return 0;
 			}
 			SDL_Color font_color = {0xFF, 0xFF, 0xFF, 0xFF};
-
-
-			SDL_Rect input_rect;
-			input_rect.x = 0;
-			input_rect.y = 0;
-			input_rect.w = 800;
-			input_rect.h = 100;
+			SDL_Rect input_rect = {0, 0, 0, 0}; 
 
 			struct InputBuffer* input_buffer = input_buffer_new(INPUT_BUFFER_SIZE);
 
@@ -72,14 +66,20 @@ int main() {
 				SDL_Surface* input_surface = TTF_RenderText_Solid(font, (const char*)input_buffer->data, font_color);
 				SDL_Texture* input_texture = SDL_CreateTextureFromSurface(renderer, input_surface);
 
+				int w;
+				int h;
+				TTF_SizeText(font, (const char*)input_buffer->data, &w, &h);
+				input_rect.w = w;
+				input_rect.h = h;
+
 				SDL_RenderCopy(renderer, input_texture, NULL, &input_rect);
 				SDL_RenderPresent(renderer);
 
 				SDL_FreeSurface(input_surface);
 				SDL_DestroyTexture(input_texture);
 			}
-			TTF_Quit();
 
+			TTF_Quit();
 			SDL_DestroyRenderer(renderer);
 			SDL_DestroyWindow(window);
 			SDL_Quit();
