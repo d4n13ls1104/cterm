22d4c7f8979e44d44a9cb4ec55a797ac1b1c3f30
d4n13ls1104 <=>
2023-07-21 03:43:48 -0400

    move definitions to header and remove linesize struct member

--- include/console.h
+++ include/console.h
@@ -6,13 +6,18 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
 
+#define WINDOW_TITLE "cterm"
+#define FONT_PATH "/home/drew/repos/c/cterm/jb_mono.ttf"
+
+#define WINDOW_WIDTH 	800
+#define WINDOW_HEIGHT 	600
+
 struct ConsoleState {
 	SDL_Window* window;
 	SDL_Renderer* renderer;
 
 	TTF_Font* font;
 	SDL_Color font_color;
-	SDL_Rect linesize;
 	bool quit;
 
 	struct InputBuffer* input_buffer;
